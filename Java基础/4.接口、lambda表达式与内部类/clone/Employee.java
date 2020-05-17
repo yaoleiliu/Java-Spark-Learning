acceee744e010c8e478d22b84c603cf35ab0d654
刘垚磊 <liuyaolei@meituan.com>
2020-05-17 09:57:32 +0800

    接口

---
+++ Java基础/4.接口、lambda表达式与内部类/clone/Employee.java
@@ -0,0 +1,10 @@
+package interfaces.clone;
+
+import java.util.*;
+
+public class Employee {
+    public static void main(String[] args){
+        String s = "20200506:81:80616:1";
+        System.out.println(s.substring(0, 8));
+    }
+}
